c7c68ace8a843254fd4631799b454bb6b1684134
Yannou - Gaming et technologies <yannnonoficiel@gmail.com>
2023-01-02 16:47:37 +0100

    HotFix !

--- Kernel/SBash/SBash.js
+++ Kernel/SBash/SBash.js
@@ -98,8 +98,8 @@ function Process(CommandStringBase) {
                 if(http.responseText != "404: Not Found"){
                   FileSystem.writeFile("/bin/"+BashCommand["SubCommands"][1]+".map"  , http.responseText);
                   for(Line in http.responseText.split("\n")){
-                    if(http.responseText.split("\n")[Line].indexOf(">") == -1){
-                      FileSystem.createDir("/" , http.responseText.split("\n")[Line]);
+                    if(http.responseText.split("\n")[Line].indexOf("DIR ") > -1){
+                      FileSystem.createDir("" , http.responseText.split("\n")[Line].replace("DIR " , ""));
                     }else{
                       var FileContent = new XMLHttpRequest();
                       FileContent.open("GET" , "https://raw.githubusercontent.com/"+FileSystem.getFileContent("/etc/repositories.conf").result.split("\n")[Repository]+"/main/Packages/"+BashCommand["SubCommands"][1] + Version +"/" + http.responseText.split("\n")[Line].split(" > ")[0] , false);
