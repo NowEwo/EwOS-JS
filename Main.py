ca43c75d7116cdf3b03925b3c27a97b0d2e78706
Wolfy <wolfyasgreywolf@gmail.com>
2023-01-14 12:36:55 +0100

    MountainGroup-1-0.0.1-Dev

--- Main.py
+++ Main.py
@@ -1,6 +1,20 @@
-import http.server
 import socketserver
+import http.server
 import webbrowser
+import websockets
+import asyncio
+import json
+
+Configuration = json.loads(open("Config.json" , "r").read())
+
+async def WebSocket():
+    uri = "ws://localhost:49351"
+    async with websockets.connect(uri) as websocket:
+        Command = await websocket.recv()
+        print(f"<<< {Command}")
+
+if(Configuration["WebSocket"] == "Enabled"):
+    asyncio.run(WebSocket())
 
 PORT = 49350
 Handler = http.server.SimpleHTTPRequestHandler
