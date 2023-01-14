934f7dd6ddeb3091e2a33dd98dfa3d26624fa428
Wolfy <wolfyasgreywolf@gmail.com>
2023-01-14 13:45:37 +0100

    FIX

--- Main.py
+++ Main.py
@@ -1,10 +1,38 @@
-import socketserver
-import http.server
-import webbrowser
+from flask import *
+import json
+import os
 
-PORT = 49350
-Handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("", PORT), Handler) as http:
-    print("serving at port", PORT)
-    webbrowser.open("http://localhost:49350")
-    http.serve_forever()
+App = Flask(__name__)
+App.config.from_object(__name__)
+
+SelariaMRConfig = json.loads(open("Config.json" , "r").read())
+
+@App.route('/', defaults={'PATH': 'Index.html'})
+@App.route('/<path:PATH>')
+def File(PATH):
+    if(request.environ['REMOTE_ADDR'] in SelariaMRConfig["Blacklist"]):
+        return send_from_directory('.', "Blacklisted.html")
+    else:
+        return send_from_directory('.', PATH.split("?")[0])
+
+@App.route('/api/', defaults={'Action': 'Infos'})
+@App.route('/api/<path:Action>')
+def API(Action):
+    if(request.environ['REMOTE_ADDR'] in SelariaMRConfig["Blacklist"]):
+        return send_from_directory('.', "Blacklisted.html")
+    else:
+        if(Action == "Infos"):
+            SuperUser = False
+            if(request.environ['REMOTE_ADDR'] in SelariaMRConfig["SuperUser"]):
+                SuperUser = True
+            ResponseData = json.dumps({
+                "Started" : True,
+                "RequestFrom" : request.environ['REMOTE_ADDR'],
+                "SuperUser" : SuperUser,
+                "SelariaFolder" : os.getcwd(),
+                "Config" : SelariaMRConfig
+            })
+            return Response(ResponseData , mimetype="application/json")
+
+if __name__ == '__main__' :
+    App.run(host="localhost" , port=49530)
