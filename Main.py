44730f36e51346997b65931681ef503d27639226
Wolfy <yannnonoficiel@gmail.com>
2023-02-15 15:43:11 +0100

    Test

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ from zipfile import ZipFile
 from uuid import uuid4
 from flask import *
 import requests
+import shutil
 import time
 import json
 import os
@@ -74,6 +75,14 @@ def Notification(Data):
     Socket.emit("Notification" , {"Text" : Data["Content"] , "From" : request.environ['REMOTE_ADDR']} , to=Data["To"])
 
 @Socket.event
+def Cast(ToIP , Code):
+    if(request.environ['REMOTE_ADDR'] in SelariaMRConfig["SuperUser"]):
+        Socket.send(Code , to=ToIP)
+    else:
+        Socket.send("You can't execute code on other clients in the server if you're not SuperUser !")
+        print("A non-superuser ip try to execute code on other clients on the server : "+request.environ['REMOTE_ADDR']+" !")
+
+@Socket.event
 def Flash(Data):
     Data = json.loads(Data)
     for File in Data["Files"]:
@@ -91,7 +100,36 @@ def Flash(Data):
 
 @Socket.event
 def Update():
-    exec(open("Updater.py" , "r").read())
+    print("Downloading latest release ...")
+    Release = requests.get("https://github.com/WolfyGreyWolf/SelariaMountainRange/archive/refs/heads/main.zip")
+    print("Writing into ZIP file ...")
+    open("../Updater.zip" , "wb").write(Release.content)
+    SMRDIR = os.getcwd().split("\\")[-1]
+    print('Extracting all the files ...')
+    with ZipFile("../Updater.zip", 'r') as ExtractableZip:
+        ExtractableZip.extractall()
+    print("Moving into the Selaria MountainRange Directory ...")
+    def merge(scr_path, dir_path):
+      files = next(os.walk(scr_path))[2]
+      folders = next(os.walk(scr_path))[1]
+      for file in files: # Copy the files
+        scr_file = scr_path + "/" + file
+        dir_file = dir_path + "/" + file
+        if os.path.exists(dir_file): # Delete the old files if already exist
+          os.remove(dir_file)
+        shutil.copy(scr_file, dir_file)
+      for folder in folders: # Merge again with the subdirectories
+        scr_folder = scr_path + "/" + folder
+        dir_folder = dir_path + "/" + folder
+        if not os.path.exists(dir_folder): # Create the subdirectories if dont already exist
+          os.mkdir(dir_folder)
+        merge(scr_folder, dir_folder)
+    merge("SelariaMountainRange-main" , "../"+SMRDIR)
+    print("Cleaning ...")
+    shutil.rmtree("SelariaMountainRange-main")
+    os.remove("../Updater.zip")
+    print("Updated !")
+    Socket.emit("Cast" , "Kernel.Reload()" , to=request.environ['REMOTE_ADDR'])
 
 @App.route('/', defaults={'PATH': 'Index.html'})
 @App.route('/<path:PATH>')
