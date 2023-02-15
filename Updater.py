ece7c928e6de28dee1f34e7ef292102ace132e8a
Wolfy <yannnonoficiel@gmail.com>
2023-02-15 15:11:53 +0100

    Fix !

--- Updater.py
+++ Updater.py
@@ -4,19 +4,24 @@ import shutil
 import os
 
 print("Downloading latest release ...")
-ZipFile = requests.get("https://github.com/WolfyGreyWolf/SelariaMountainRange/archive/refs/heads/main.zip")
-print("Writing into ZIP file !")
-open("../Updater.zip" , "wb").write(ZipFile.content)
+
+Release = requests.get("https://github.com/WolfyGreyWolf/SelariaMountainRange/archive/refs/heads/main.zip")
+
+print("Writing into ZIP file ...")
+
+open("../Updater.zip" , "wb").write(Release.content)
 
 SMRDIR = os.getcwd().split("\\")[-1]
 
 print('Extracting all the files ...')
 
-with ZipFile("../Updater.zip", 'r') as Zip:
-    Zip.extractall()
+with ZipFile("../Updater.zip", 'r') as ExtractableZip:
+    ExtractableZip.printdir()
+    ExtractableZip.extractall()
 
-print("Moving into the Selaria MountainRange Directory !")
+print("Moving into the Selaria MountainRange Directory ...")
 
-shutil.move("../Updater/SelariaMountainRange-main" , "../"+SMRDIR)
+for Element in os.listdir("../Updater/SelariaMountainRange-main"):
+    shutil.move("../Updater/SelariaMountainRange-main/"+Element , "../"+SMRDIR+"/"+Element)
 
 print("Updated !")
